ee35df1941b6ebe8933015bff4e69230cb1ae623
Tayyab Nisar <tayyabnisar@edraaksystems.com>
2023-03-21 15:14:26 +0500

    first-commit

---
+++ frontend/src/redux/authSlice/apiCalls.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { toast } from "react-toastify";
+import { loginStart, loginSuccess, loginFailure } from "./index";
+import dotenv from 'dotenv';
+
+
+export const login = async (user, dispatch) => {
+	dotenv.config();
+	const apiUrl = `${process.env.REACT_APP_API_URL}` + "/login";
+
+	dispatch(loginStart());
+	try {
+		const url = apiUrl;
+		const { data } = await axios.post(url, user);
+
+		const decodeData = jwt_decode(data.data);
+		if (!decodeData.isAdmin) {
+			toast.error("You don't have access");
+			dispatch(loginFailure());
+			return;
+		}
+		toast.success(data.message);
+		dispatch(loginSuccess({ ...decodeData, token: data.data }));
+		window.location = "/";
+	} catch (error) {
+		dispatch(loginFailure());
+		if (error.response.status >= 400 && error.response.status < 500) {
+			toast.error(error.response.data.message);
+		}
+	}
+};
